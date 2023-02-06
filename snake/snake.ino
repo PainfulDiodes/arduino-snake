d9be5a5ec664084c50390765f274f9fa2659329b
Stephen Willcock <stephenwillcock@yahoo.co.uk>
2023-02-06 17:33:25 +0000

    Update snake.ino
    Add a bunch of constants
    Fix flicker

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,3 +1,6 @@
+// Snake classic arcade came on Arduino UNO with Adafruit ILI9341 touchscreen TFT shield (capacitive)
+// https://learn.adafruit.com/adafruit-2-8-tft-touch-shield-v2
+
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
@@ -7,21 +10,48 @@
 #define TFT_DC 9
 #define TFT_CS 10
 
-#define MOTION_NONE 0
-#define MOTION_UP 1
-#define MOTION_DOWN 2
-#define MOTION_LEFT 3
-#define MOTION_RIGHT 4
-
-int direction = 0;
-int cell_x = 16;
-int cell_y = 12;
-
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 // The FT6206 uses hardware I2C (SCL/SDA)
 Adafruit_FT6206 ts = Adafruit_FT6206();
+
+#define MOTION_NONE  0
+#define MOTION_UP    1
+#define MOTION_DOWN  2
+#define MOTION_LEFT  3
+#define MOTION_RIGHT 4
+
+// Touchscreen resolution is 320x240
+#define X_MAX_PX 320
+#define Y_MAX_PX 240
+
+// For controlling the snake we divide the screen in to 3x3 touch zones:
+//   1 2 3
+//   4 5 6
+//   7 8 9
+// Zone 4 = go left
+// Zone 6 = go right
+// Zone 2 = go up
+// Zone 8 = go down
+
+// Touch pixel ranges:
+#define X_THIRD 106
+#define X_TWO_THIRD 213
+#define Y_THIRD 80
+#define Y_TWO_THIRD 160
+
+// For display, we divide the entire screen into 10x10 px cells (32x24 grid)
+#define X_MAX_CELL 32
+#define Y_MAX_CELL 24
+#define X_START_CELL 16
+#define Y_START_CELL 12
+#define CELL_SIZE 10
+
+int motion = MOTION_NONE;
+int x_cell_pos = X_START_CELL;
+int y_cell_pos = Y_START_CELL;
+
 
 void setup() {
   Serial.begin(9600);
@@ -51,7 +81,7 @@ void setup() {
    tft.fillScreen(ILI9341_BLACK);
    tft.setRotation(1); 
 
-  setCell(cell_x,cell_y);
+  setCell(x_cell_pos,y_cell_pos);
 }
 
 
@@ -63,21 +93,18 @@ void loop(void) {
     TS_Point p = ts.getPoint(); 
     // rotate coordinate system
     // flip it around to match the screen.
-    p.x = map(p.x, 0, 240, 240, 0);
-    p.y = map(p.y, 0, 320, 320, 0);
+    p.x = map(p.x, 0, Y_MAX_PX, Y_MAX_PX, 0);
+    p.y = map(p.y, 0, X_MAX_PX, X_MAX_PX, 0);
     int y = tft.height() - p.x;
     int x = p.y;
 
-    // x: 0-105 106-214 215-320
-    // y: 0-80 81-159 160-240
+    if(     x <  X_THIRD     && y >= Y_THIRD     && y <  Y_TWO_THIRD) motion = MOTION_LEFT;
+    else if(x >= X_TWO_THIRD && y >= Y_THIRD     && y <  Y_TWO_THIRD) motion = MOTION_RIGHT;
+    else if(x >= X_THIRD     && x <  X_TWO_THIRD && y >= Y_TWO_THIRD) motion = MOTION_DOWN;
+    else if(x >= X_THIRD     && x <  X_TWO_THIRD && y <  Y_THIRD    ) motion = MOTION_UP;
+    else                                                              motion = MOTION_NONE;
 
-    if(x<106 && y>80 && y<160) direction=MOTION_LEFT;
-    else if(x>214 && y>80 && y<160) direction=MOTION_RIGHT;
-    else if(x>105 && x<215 && y>159) direction=MOTION_DOWN;
-    else if(x>105 && x<215 && y<81) direction=MOTION_UP;
-    else direction=MOTION_NONE;
-
-    switch (direction) {
+    switch (motion) {
       case MOTION_LEFT:
         Serial.print("Left");
         break;
@@ -111,42 +138,42 @@ void loop(void) {
 }
 
 void setCell(int x, int y) {
-  tft.fillRect(x*10, y*10, 10, 10, ILI9341_GREEN);
+  tft.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, ILI9341_GREEN);
 }
 
 void unsetCell(int x, int y) {
-  tft.fillRect(x*10, y*10, 10, 10, ILI9341_BLACK);
+  tft.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, ILI9341_BLACK);
 }
 
 void move() {
-  int x = cell_x;
-  int y = cell_y;
+  int x = x_cell_pos;
+  int y = y_cell_pos;
 
-  switch (direction) {
+  switch (motion) {
     case MOTION_LEFT:
-      cell_x--;
+      x_cell_pos--;
       break;
     case MOTION_RIGHT:
-      cell_x++;
+      x_cell_pos++;
       break;
     case MOTION_UP:
-      cell_y--;
+      y_cell_pos--;
       break;
     case MOTION_DOWN:
-      cell_y++;
+      y_cell_pos++;
       break;
     case MOTION_NONE:
-      break;
+      return;
     default:
       break;
   }
 
-  if(cell_x<0) cell_x = 32;
-  if(cell_x>32) cell_x = 0;
-  if(cell_y<0) cell_y = 24;
-  if(cell_y>24) cell_y = 0;
+  if(x_cell_pos < 0)  x_cell_pos = X_MAX_CELL;
+  if(x_cell_pos > X_MAX_CELL) x_cell_pos = 0;
+  if(y_cell_pos < 0)  y_cell_pos = Y_MAX_CELL;
+  if(y_cell_pos > Y_MAX_CELL) y_cell_pos = 0;
 
   unsetCell(x,y);
-  setCell(cell_x,cell_y);
+  setCell(x_cell_pos,y_cell_pos);
 
 }
